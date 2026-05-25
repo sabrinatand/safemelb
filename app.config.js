9d056582fca0e8f9254360959fae70fe3e522e38
sabrinatand <sabrina.agnes.tandika@gmail.com>
2026-05-25 16:27:56 +1000

    fix: remove exposed API key, use env variable via app.config.js

---
+++ app.config.js
@@ -0,0 +1,29 @@
+export default {
+  expo: {
+    name: 'safemelb',
+    slug: 'safemelb',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+    },
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_KEY,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+      },
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_KEY,
+        },
+      },
+    },
+  },
+};
